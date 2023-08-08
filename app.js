5ad45875c6f9af41d1ca6361429074977f0a4e49
mfadhili <mfadhili@github.com>
2023-08-09 01:38:26 +0300

    feat add mainRouter

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 
+const aagRouter = require('./src/routes/mainRouter');
+
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use('/ag', agRouter );
 
 app.listen(port, () => {
     console.log(`Agri netcom listening at http://localhost:${port}`);
